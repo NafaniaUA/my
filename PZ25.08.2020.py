0662e46badfe254c771d90ea650437daa42b3e44
Anastasiya Sushma <51820854+NafaniaUA@users.noreply.github.com>
2020-08-26 13:33:27 +0300

    Перероблено

--- PZ25.08.2020.py
+++ PZ25.08.2020.py
@@ -1,53 +1,73 @@
- 
-class Empoloyer(Departament):
-    def __init__ (self, first_name, second_name, age, sallary, job, bonus):
-        
+MAX_BONUS = 500
+class Employer:
+    def __init__ (self, first_name, second_name, age, sallary, job, bonus=0):
+
         self.first_name = first_name
         self.second_name = second_name
         self.age = age
         self.sallary = sallary
         self.job = job
-        self.bonus = 0
-        
-    def __prn__(self):
-        return f'Employer',{self.first_name}, {self.second_name}
+        self.bonus = bonus
+
+    def __repr__(self):
+        return f'Employer({self.first_name, self.second_name})'
+    
+    @property
+    def total_sallary(self):
+        return self.bonus + self.sallary
+    
+    def add_bonus (self, bonus):
+        if bonus > MAX_BONUS:
+            print("Ви перевищили максимальний бонус")
+        else:
+            self.bonus+=bonus
+
 
 
 class Departament:
-    def __init__ (self, name):
+    def __init__ (self, name, emploers = []):
         self.name = name
-        self.employers[]
+        self.employers = emploers
     
+    def __iter__(self):
+        return (e for e in self.employers)
+
     def add_employer(self, employer):
         self.employers.append(employer)
 
 
 
 def vvid():
-    emloyer1 = Employer(
+    employer1 = Employer(
         first_name = "John",
         second_name ="Smith",
-        age = 34
-        sallary = 1000
-        job = "Engineer"
+        age = 34,
+        sallary = 1000,
+        job = "Engineer",
+        #bonus=0
 
     )
 
-    emloyer2 = Employer(
+    employer2 = Employer(
         first_name = "Edward",
         second_name ="Norton",
-        age = 29
-        sallary = 1500
-        job = "Manager"
-        
+        age = 29,
+        sallary = 1500,
+        job = "Manager",
+        #bonus=0
+
     )
     departament = Departament(name = "New")
-    departament.add_employer(empoloyer1)
-    departament.add_employer(empoloyer2)
+    employer1.add_bonus(6000)
+    departament.add_employer(employer1)
+    departament.add_employer(employer2)
 
-    for i in departament:
-        print (i)
+    print(departament.name)
+    for e in departament:
+        print(e,'sallary ', e.total_sallary)
+
+    
 
 
 vvid()
-# Я ще роблю, тут помилка, я просто кидаю перший варіант :)
+
